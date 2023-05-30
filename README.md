6682c1e99e680e5001b3c29048eef17874cc4a07
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-05-30 14:22:32 +0300

    done
    homework done

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# node-js
+# Виконання завдання
+
+```
+## Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+node index.js --action="list"
+```
+
+![listContacts](./images/list-contacts.jpg)
+
+```
+## Отримуємо контакт по id
+
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+
+```
+
+![getContactByID](./images/get-contact-by-id.jpg)
+
+```
+## Додаємо контакт
+
+node index.js --action="add" --name Mango --email mango@gmail.com --phone
+322-22-22
+```
+
+![add](./images/add-contact.jpg)
+
+```
+## Видаляємо контакт
+
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+```
+
+![remove](./images/remove-contact.jpg)
---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require('fs').promises;
+const { nanoid } = require('nanoid');
+const path = require('path');
+
+const contactsPath = path.join(__dirname, '/db/contacts.json');
+
+// returns a list of contacts
+async function listContacts() {
+  const fileText = await fs.readFile(contactsPath);
+  return JSON.parse(fileText);
+}
+
+// return contact by id
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find(item => item.id === contactId);
+  return contact || null;
+}
+
+// remove contact by id
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+// add new contact
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
