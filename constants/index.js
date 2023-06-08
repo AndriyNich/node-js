d2359d409374babcfe9be51fddb87f8c40d46d52
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-06-08 20:04:08 +0300

    user
    create user model & schems

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+const emailRegexp = require("./regexp");
+
+module.exports = {
+  emailRegexp,
+};
---
+++ models/user/schema.Joi.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+const { emailRegexp } = require("../../constants");
+
+const registerSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  subscription: Joi.string(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().patern(email.emailRegexp).required(),
+  password: Joi.string().required(),
+});
+
+module.exports = {
+  registerSchema,
+  loginSchema,
+};
