8e195077f40e9c958c92e373d15a065590e8e5e5
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-06-09 18:59:50 +0300

    create ctrlWrapper

--- controllers/auth/current.js
+++
@@ -1,3 +0,0 @@
-const current = async (req, res) => {};
-
-module.exports = current;
--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,13 +1,15 @@
+const { ctrlWrapper } = require("../../helpers");
+
 const register = require("./register");
 const login = require("./login");
-const current = require("./current");
+const getCurrent = require("./getCurrent");
 const logout = require("./logout");
 
 // ToDo: control wrapper
 
 module.exports = {
-  register,
-  login,
-  current,
-  logout,
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
 };
