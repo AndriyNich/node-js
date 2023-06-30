c41bdbe7a116a6cb29c7c7c3474efb7591a5f894
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-06-30 16:55:13 +0300

    delete file httpError.js

--- helpers/httpError.js
+++
@@ -1,9 +0,0 @@
-const errorMessageList = require("../constants");
-
-const HttpError = (status, message = errorMessageList[status]) => {
-  const error = new Error(message);
-  error.status = status;
-  return error;
-};
-
-module.exports = HttpError;
