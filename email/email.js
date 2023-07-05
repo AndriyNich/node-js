c50c0eadee4352efc677692ae6c9b1523d99393a
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-07-05 09:26:55 +0300

    sendgrid

--- email/email.js
+++ email/email.js
@@ -1,9 +1,8 @@
 const sgmail = require("@sendgrid/mail");
 require("dotenv").config();
 
-// const { SENDGRID_API_KEY } = process.env;
-const SENDGRID_API_KEY =
-  "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";
+const { SENDGRID_API_KEY } = process.env;
+
 sgmail.setApiKey(SENDGRID_API_KEY);
 
 const email = {
